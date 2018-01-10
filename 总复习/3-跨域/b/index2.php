95e724d9965f786ef9ff7124ee9aeb7392a6ef6e
houweix <houwei19960927@126.com>
2018-01-10 16:58:56 +0800

    css3选择器练习

--- 总复习/3-跨域/b/index2.php
+++
@@ -1,8 +0,0 @@
-<?php
-    $name = $_GET['name'];
-    echo $name;
-
-
-?>
-
-<iframe src="http://www.a.com/index6.php?age=20" frameborder="0"></iframe>
--- 总复习/3-跨域/b/index4.php
+++
@@ -1,9 +0,0 @@
-<?php
-
-    header("Access-Control-Allow-Origin:http://www.a.com");
-
-    $age = $_GET['age'];
-    echo $age;
-
-
-?>
